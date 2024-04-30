be64dcae5e9b41f34460e5451a118fc91c40fca4
acharanpahari <166602396+acharanpahari@users.noreply.github.com>
2024-04-30 07:17:20 +0530

    Update data.tf

--- Applications/web-app/data.tf
+++ Applications/web-app/data.tf
@@ -3,8 +3,8 @@ data "azurerm_key_vault_secret" "username" {
   key_vault_id = "/subscriptions/f52a4403-4d69-45d0-babf-63321e10cc70/resourceGroups/kvaults-rg/providers/Microsoft.KeyVault/vaults/creds-kv"
 }
 
-data "azurerm_key_vault_secret" "password" {
-  name         = "password"
+data "azurerm_key_vault_secret" "ssh-public-key" {
+  name         = "ssh-public-key"
   key_vault_id = "/subscriptions/f52a4403-4d69-45d0-babf-63321e10cc70/resourceGroups/kvaults-rg/providers/Microsoft.KeyVault/vaults/creds-kv"
 }
 
