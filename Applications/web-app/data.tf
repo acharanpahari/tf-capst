f7a033e2aaf01c6f4d51d18bb15bca97dce45bb5
acharanpahari <166602396+acharanpahari@users.noreply.github.com>
2024-05-04 15:33:05 +0000

    corrected data block

--- Applications/web-app/data.tf
+++ Applications/web-app/data.tf
@@ -1,12 +1,13 @@
 
-data "azurerm_key_vault" "example" {
+data "azurerm_key_vault" "key_vault" {
   name                = "mykeyvault"
   resource_group_name = "kvaults-rg"
 }
 
 data "azurerm_key_vault_secret" "username" {
   name         = "user"
-  key_vault_id = data.azurerm_key_vault.id
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
 
 data "azurerm_key_vault_secret" "ssh-public-key" {
   name         = "ssh-public-key"
