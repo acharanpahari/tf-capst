62b214b1b5d28f2b2b484fac5c8776dad24c0964
acharanpahari <166602396+acharanpahari@users.noreply.github.com>
2024-05-04 15:25:54 +0000

    added data block for key vault

--- Applications/web-app/data.tf
+++ Applications/web-app/data.tf
@@ -1,7 +1,12 @@
+
+data "azurerm_key_vault" "example" {
+  name                = "mykeyvault"
+  resource_group_name = "kvaults-rg"
+}
+
 data "azurerm_key_vault_secret" "username" {
   name         = "user"
-  key_vault_id = "/subscriptions/f52a4403-4d69-45d0-babf-63321e10cc70/resourceGroups/kvaults-rg/providers/Microsoft.KeyVault/vaults/creds-kv"
-}
+  key_vault_id = data.azurerm_key_vault.id
 
 data "azurerm_key_vault_secret" "ssh-public-key" {
   name         = "ssh-public-key"
